732073db51b8403c6c003de9d3d08153821f2812
Jose <lopez.jose124@icloud.com>
2025-05-29 19:10:08 -0400

    chore(backend): initial Spring Boot project setup

---
+++ backend/app/src/test/java/com/JoseJCodes/Budget_app/BudgetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JoseJCodes.Budget_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
